4c7a3a2dbdf18d069b927f471f35ee894414f3cd
Akshay <jinmori58690@gmail.com>
2024-02-25 00:07:02 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,8 +7,8 @@ load_dotenv()
 
 
 #Basic-Setup
-OWNER_ID = int(getenv("OWNER_ID, 6295947116"))
-LOGGER_ID = int(getenv("LOGGER_ID, -1001947276976"))
+OWNER_ID = 6295947116
+LOGGER_ID = -1001947276976
 
 #Bot-Setup
 BOT_TOKEN = getenv("6189399999:AAFaN1Z_TD3kq16I87EMeuD_TMu23Lpqq_k")
@@ -17,14 +17,14 @@ BOT_TOKEN = getenv("6189399999:AAFaN1Z_TD3kq16I87EMeuD_TMu23Lpqq_k")
 MONGO_DB_URI = getenv("mongodb+srv://gotouhitoriii:D0ybqrDVq6MuKiqh@suika.al7oyue.mongodb.net/?retryWrites=true&w=majority&appName=Suika", None)
 
 #Assistant-Setup
-API_ID = int(getenv("API_ID, 22069422"))
-API_HASH = getenv("API_HASH, a2b3891c2041a4326fcb4e77b54fb32a")
+API_ID = 22069422
+API_HASH = a2b3891c2041a4326fcb4e77b54fb32a
 
-STRING1 = getenv("AQExX0YAsiyPsOjlKh7SMf9FEeIBVaI_9FWLYz4fEgATyid7kl7DORMSqKiaqFkQMi3j4lBFlotE3GrZ0ORSNpu-Ry-A6rgA1WCheVEE3rHKtv1wMkZ9oZU_u7NZxGyRWzcoiS-ZEnUo3FX0ygPChG-s8kbPzmwyhjsP5vR19AbcCc0Q4ZiyVaUCbPg0a0PABp__Jm76OmKTaitV4uY2cVR6WK6K37DEUOXcpn3_ow_78tysZcN-jifh30fLYaT_kXbYdny-9b4ZgQ1jjAttHCigJmwkDo4aYHDkUyK5L3pS0nPokvQWQ4oBNulpXnql8kr1cSkDTN8SbHmlL5IHypKKAvJ0hAAAAAFqyyBcAA")
-STRING2 = getenv("AQExX0YAsdC2f4A841hwcQfoUgM4Ck1m1_LjwofrtE1HhCs2GkKo_Uvdb4E5JnhxAY80bJxleUq9NkOZ8n5xhmdZU3VCYHuFtk9Jy_gccvCzmwwABbR_QH8wNLZTyLMwdM75CCJvjCdcP1okVqNrhm7T9GlBVyNmS0Lv1JACENVuDda-gp9wa4sAiCVxwqMYSWgtXbkgKBBBxrBfyxs2rymlNm0wECWw7ZQUmBsbcCXfQZzrCV56bgDUCLVGo6WwMQCQwqaxeb4RKDOENyB4IM-OZQtYFCXUVlPs90lAOM37hCU8ug7ITqqlbQVeFx_gTTiEn8bi0nJ-3ZThh_OnoKopK9lk8AAAAAFqyyBcAA")
-STRING3 = getenv("AQExX0YAPGP26w-NRrCCo6NWSDsFK-0g6jVl4jItVXjnU-ODRxjwzWmm4R7zPCuLYibQBr3CWHFDNz8oef4W4bG6ytFfjEMJ1Bs3CL1_F2aEml3KmYaQpPfgDyW_RSXbxsZSVSUxzbxzlgnYoED7dW83G00CUlmPBmp8Brz8JgygKhnzfXa64HeUzRm_T9dnhOmh1Gc8MsGNOx8aXf31bwo1zj4ghJdpOwhmPoAjvw2tSgs0myWgiCkZ5ZqRWX9UEj-H6wVGdLNMY171xJYAdolRs3owZzpYznu51JrKsImRULTdbwvSpSKMtG0l8eGl4EUfzR7UcLwjnxajH6K07tC9Eh8exAAAAAFqyyBcAA")
-STRING4 = getenv("AQExX0YAXi9wzFFWfXAQwl_0_YVriQSPTMfxeGiUcpSu44Wken5yNMmsHA3zOSc-YoajpHcBm34pG8Dlh-9t-LmJaGB9fgL2sS_w6KXEzJMEgrXLFHRLmO7v7MNPS5ap3zT-Eq_GDcCQiWHrBZtwhnOSwn1rmjv2p80vBeRzNQLosz67RMHCfIfSfFCytXQf6crsZcQLsyRHMfQ2MIn06vhXF_eaxoyeCcBiZq_km2RpizertWLjqo5ET0IXk1H5Dzu06WlGgXFJ3PgLvvUJz-nPXNBHztjzhb3lym17Bjwp8Z3Ix0AkbnmUufMBIT6mXKgyT-ImNumEK7jlGmAHQAijhYxR2gAAAAFqyyBcAA")
-STRING5 = getenv("AQExX0YAYQSaxN4mnSskvbScI-1ZWPU_KOS1ZqDbQEYe9C_fpV4EpI0r90A3kwwQ0RitiGU13PXKhrRQQHgISXuWlPkll9AtODbmXgABLy_nLk-z42BV0fVtbgxooU9Ei6i8iycNMBslI5VzLIh3e3NI5I3CCSO3oww7gVF5SIBy-EhNqb00X0vqskEsJS7lMa44xv8aZyb7zgXGeVOXcWk7DUzbmJ72uXcxrSMEoyr1V5aF1nbjjhiPwAZbjBXaHcuUCrWajujfvZlYxgfNA5jwkmXV183jZGSWnExKMVemYGV6X-n_rCzxyu3Kg5P86rnWsq481kNYaIMgiadW-v2piMANNQAAAAFqyyBcAA")
+STRING1 = AQExX0YAsiyPsOjlKh7SMf9FEeIBVaI_9FWLYz4fEgATyid7kl7DORMSqKiaqFkQMi3j4lBFlotE3GrZ0ORSNpu-Ry-A6rgA1WCheVEE3rHKtv1wMkZ9oZU_u7NZxGyRWzcoiS-ZEnUo3FX0ygPChG-s8kbPzmwyhjsP5vR19AbcCc0Q4ZiyVaUCbPg0a0PABp__Jm76OmKTaitV4uY2cVR6WK6K37DEUOXcpn3_ow_78tysZcN-jifh30fLYaT_kXbYdny-9b4ZgQ1jjAttHCigJmwkDo4aYHDkUyK5L3pS0nPokvQWQ4oBNulpXnql8kr1cSkDTN8SbHmlL5IHypKKAvJ0hAAAAAFqyyBcAA
+STRING2 = AQExX0YAsdC2f4A841hwcQfoUgM4Ck1m1_LjwofrtE1HhCs2GkKo_Uvdb4E5JnhxAY80bJxleUq9NkOZ8n5xhmdZU3VCYHuFtk9Jy_gccvCzmwwABbR_QH8wNLZTyLMwdM75CCJvjCdcP1okVqNrhm7T9GlBVyNmS0Lv1JACENVuDda-gp9wa4sAiCVxwqMYSWgtXbkgKBBBxrBfyxs2rymlNm0wECWw7ZQUmBsbcCXfQZzrCV56bgDUCLVGo6WwMQCQwqaxeb4RKDOENyB4IM-OZQtYFCXUVlPs90lAOM37hCU8ug7ITqqlbQVeFx_gTTiEn8bi0nJ-3ZThh_OnoKopK9lk8AAAAAFqyyBcAA
+STRING3 = AQExX0YAPGP26w-NRrCCo6NWSDsFK-0g6jVl4jItVXjnU-ODRxjwzWmm4R7zPCuLYibQBr3CWHFDNz8oef4W4bG6ytFfjEMJ1Bs3CL1_F2aEml3KmYaQpPfgDyW_RSXbxsZSVSUxzbxzlgnYoED7dW83G00CUlmPBmp8Brz8JgygKhnzfXa64HeUzRm_T9dnhOmh1Gc8MsGNOx8aXf31bwo1zj4ghJdpOwhmPoAjvw2tSgs0myWgiCkZ5ZqRWX9UEj-H6wVGdLNMY171xJYAdolRs3owZzpYznu51JrKsImRULTdbwvSpSKMtG0l8eGl4EUfzR7UcLwjnxajH6K07tC9Eh8exAAAAAFqyyBcAA
+STRING4 = AQExX0YAXi9wzFFWfXAQwl_0_YVriQSPTMfxeGiUcpSu44Wken5yNMmsHA3zOSc-YoajpHcBm34pG8Dlh-9t-LmJaGB9fgL2sS_w6KXEzJMEgrXLFHRLmO7v7MNPS5ap3zT-Eq_GDcCQiWHrBZtwhnOSwn1rmjv2p80vBeRzNQLosz67RMHCfIfSfFCytXQf6crsZcQLsyRHMfQ2MIn06vhXF_eaxoyeCcBiZq_km2RpizertWLjqo5ET0IXk1H5Dzu06WlGgXFJ3PgLvvUJz-nPXNBHztjzhb3lym17Bjwp8Z3Ix0AkbnmUufMBIT6mXKgyT-ImNumEK7jlGmAHQAijhYxR2gAAAAFqyyBcAA
+STRING5 = AQExX0YAYQSaxN4mnSskvbScI-1ZWPU_KOS1ZqDbQEYe9C_fpV4EpI0r90A3kwwQ0RitiGU13PXKhrRQQHgISXuWlPkll9AtODbmXgABLy_nLk-z42BV0fVtbgxooU9Ei6i8iycNMBslI5VzLIh3e3NI5I3CCSO3oww7gVF5SIBy-EhNqb00X0vqskEsJS7lMa44xv8aZyb7zgXGeVOXcWk7DUzbmJ72uXcxrSMEoyr1V5aF1nbjjhiPwAZbjBXaHcuUCrWajujfvZlYxgfNA5jwkmXV183jZGSWnExKMVemYGV6X-n_rCzxyu3Kg5P86rnWsq481kNYaIMgiadW-v2piMANNQAAAAFqyyBcAA
 AUTO_LEAVING_ASSISTANT = bool(getenv("True")) 
 
 
@@ -58,20 +58,20 @@ confirmer = {}
 
 
 START_IMG_URL = getenv(
-    "START_IMG_URL", "https://associated-lilyan-thetrueheavensequal.koyeb.app/api/file/share?hash=14B143AFF2B249C7CD31CFDD201CF93F6E4E90105511103045EB9E2C2BFAAB25&expire=1708799578&path=Suika%252Fstart.png"
+    "START_IMG_URL", "https://te.legra.ph/file/192f102f7d6a7a4561c82.jpg"
 )
 PING_IMG_URL = getenv(
-    "PING_IMG_URL", "https://associated-lilyan-thetrueheavensequal.koyeb.app/api/file/share?hash=E3E6BB295C1C3F0C51BE42E87099362F980B5187E7682DCEB48CEC445BE6402B&expire=1708799614&path=Suika%252Fping.jpeg"
+    "PING_IMG_URL", "https://te.legra.ph/file/5ca09da4f777b791cc128.jpg"
 )
-STATS_IMG_URL = "https://associated-lilyan-thetrueheavensequal.koyeb.app/api/file/share?hash=BBD6CAEA89BAFC7B599BE37154C2CA22A6E70717653932FE304F63AF858C48D2&expire=1708799641&path=Suika%252Fstats.jpeg"
-PLAYLIST_IMG_URL = "https://associated-lilyan-thetrueheavensequal.koyeb.app/api/file/share?hash=04C89FEA961D547F3A2A2D92F146ABC640A46F72721780BF982B2720A5FACC9F&expire=1708799664&path=Suika%252Fplaylist.jpeg"
-TELEGRAM_AUDIO_URL = "https://associated-lilyan-thetrueheavensequal.koyeb.app/api/file/share?hash=0FD880067E09EF2921B81B9B641E640E584D1187BB0D935EA35C235A7EADDF26&expire=1708799684&path=Suika%252Ftelegram.png"
-TELEGRAM_VIDEO_URL = "https://associated-lilyan-thetrueheavensequal.koyeb.app/api/file/share?hash=0FD880067E09EF2921B81B9B641E640E584D1187BB0D935EA35C235A7EADDF26&expire=1708799684&path=Suika%252Ftelegram.png"
-STREAM_IMG_URL = "https://associated-lilyan-thetrueheavensequal.koyeb.app/api/file/share?hash=89F7D553212215C46038AB92E9D154305988F0157982057DFBCF0CC07D7F85F4&expire=1708799713&path=Suika%252Fstream.png"
-YOUTUBE_IMG_URL = "https://associated-lilyan-thetrueheavensequal.koyeb.app/api/file/share?hash=03E23116EED9D5B70076433882A699FFB90813738032A0FA3DBF56D425809AC8&expire=1708799719&path=Suika%252Fyoutube.png"
-SPOTIFY_ARTIST_IMG_URL = "https://associated-lilyan-thetrueheavensequal.koyeb.app/api/file/share?hash=64700DA8FF6082D5D1FB8447993693EA5829C6AE9515605EECDE83A172ED4DBB&expire=1708799724&path=Suika%252Fspotify.png"
-SPOTIFY_ALBUM_IMG_URL = "https://associated-lilyan-thetrueheavensequal.koyeb.app/api/file/share?hash=64700DA8FF6082D5D1FB8447993693EA5829C6AE9515605EECDE83A172ED4DBB&expire=1708799724&path=Suika%252Fspotify.png"
-SPOTIFY_PLAYLIST_IMG_URL = "https://associated-lilyan-thetrueheavensequal.koyeb.app/api/file/share?hash=64700DA8FF6082D5D1FB8447993693EA5829C6AE9515605EECDE83A172ED4DBB&expire=1708799724&path=Suika%252Fspotify.png"
+STATS_IMG_URL = "https://te.legra.ph/file/e1ba3681120abf59e99ef.jpg"
+PLAYLIST_IMG_URL = "https://te.legra.ph/file/f4f35a6b0409d13ef2432.jpg"
+TELEGRAM_AUDIO_URL = "https://te.legra.ph/file/a87bfaa9891a5eeee1c0b.jpg"
+TELEGRAM_VIDEO_URL = "https://te.legra.ph/file/a87bfaa9891a5eeee1c0b.jpg"
+STREAM_IMG_URL = "https://te.legra.ph/file/015bc41c2ca1c2f78d1c9.jpg"
+YOUTUBE_IMG_URL = "https://te.legra.ph/file/b599dbbe7e487b79ed8ba.jpg"
+SPOTIFY_ARTIST_IMG_URL = "https://te.legra.ph/file/fe33856d0222b08beb04c.jpg"
+SPOTIFY_ALBUM_IMG_URL = "https://te.legra.ph/file/fe33856d0222b08beb04c.jpg"
+SPOTIFY_PLAYLIST_IMG_URL = "https://te.legra.ph/file/fe33856d0222b08beb04c.jpg"
 
 
 def time_to_seconds(time):
